6ff8e6a117968e89728c5374aa3b9f62ed404feb
Gilles Debunne <gilles.debunne@gmail.com>
2013-09-04 12:09:54 +0200

    Step 2 : displaying some data

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -1,10 +1,5 @@
 'use strict';
 
-angular.module('myAppApp')
-  .controller('MainCtrl', function ($scope) {
-    $scope.awesomeThings = [
-      'HTML5 Boilerplate',
-      'AngularJS',
-      'Karma'
-    ];
-  });
+angular.module('myAppApp').controller('MainCtrl', function ($scope) {
+    $scope.wines = [ {name:'Red'}, {name:'White'}, {name:'Rosé'} ];
+});
